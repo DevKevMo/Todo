4d8923fce3b6ff6d854e8509d3ad4fd9cbe899fe
K.Lab <Kevin-Moritz-2001@outlook.de>
2023-06-07 11:47:26 +0200

    file structure and title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css';
+import React from "react";
+import { Toaster } from "react-hot-toast";
+import "@fontsource/poppins";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
+import PageTitle from "./components/PageTitle";
 
 function App() {
   return (
-   Hello World
+    <>
+      <div className="container">
+        <PageTitle>TODO List</PageTitle>
+      </div>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          style: {
+            fontSize: "1.4rem",
+          },
+        }}
+      />
+    </>
   );
 }
 
